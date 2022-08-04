30c70b749fcbca19a0e9b73c0970bb55c4c17ba6
Benaouali Mokhtaria <benaoualimokhtaria@hotmail.com>
2022-08-04 22:18:23 +0200

    Create componenet

---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Exchanges = () => {
+    return (
+        <div>
+Exchanefgdddddddddddddddgsdddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+fskdfjldslmkglmsd
+ksdjdklglsdlg
+nfskldqfkdlqsglm
+ksmqlslldddkkkkkkkkkkkkkkkkkkkkkkk
+        </div>
+    );
+};
+
+export default Exchanges;
